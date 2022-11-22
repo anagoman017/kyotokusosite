b2ee646f41696d157c445fa9a7df0b7a8feee4bd
DESKTOP-ADV4H5H\Kazuho <hochiko0107@gmail.com>
2022-11-22 18:14:34 +0900

    メニュー追加

--- kyotokusosite.js
+++ kyotokusosite.js
@@ -1,41 +1,80 @@
 //ロゴフェードイン
-$(function() {
-	setTimeout(function(){
-		$('.logo_fadein p').fadeIn(1000);
-	},500);
-	setTimeout(function(){
-		$('.logo_fadein').fadeOut(1000);
-	},2500);
+$(function () {
+  setTimeout(function () {
+    $('.logo_fadein p').fadeIn(1000);
+  }, 500);
+  setTimeout(function () {
+    $('.logo_fadein').fadeOut(1000);
+  }, 2500);
+});
+
+//ハンバーガーメニュー
+
+$(".openbtn").click(function () {//ボタンがクリックされたら
+  $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+  $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+  $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+  $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
 });
 
 //ページスクロール
 $.scrollify({
-    section : ".box",//1ページスクロールさせたいエリアクラス名
-    scrollbars:"false",//スクロールバー表示・非表示設定
-    interstitialSection : "#header,#footer",//ヘッダーフッターを認識し、1ページスクロールさせず表示されるように設定
-    easing: "swing", // 他にもlinearやeaseOutExpoといったjQueryのeasing指定可能
-      scrollSpeed: 300, // スクロール時の速度
-    
-    //以下、ページネーション設定
-    before:function(i,panels) {
-      var ref = panels[i].attr("data-section-name");
-        $(".pagination .active").removeClass("active");
-        $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
-      },
-      afterRender:function() {
-        var pagination = "<ul class=\"pagination\">";
-        var activeClass = "";
-        $(".box").each(function(i) {//1ページスクロールさせたいエリアクラス名を指定
-          activeClass = "";
-          if(i===$.scrollify.currentIndex()) {
-            activeClass = "active";
-          }
-          pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
-        });
-        pagination += "</ul>";
-  
-        $("#box1").append(pagination);//はじめのエリアにページネーションを表示
-        $(".pagination a").on("click",$.scrollify.move);
+  section: ".box",//1ページスクロールさせたいエリアクラス名
+  scrollbars: "false",//スクロールバー表示・非表示設定
+  interstitialSection: "#header,#footer",//ヘッダーフッターを認識し、1ページスクロールさせず表示されるように設定
+  easing: "swing", // 他にもlinearやeaseOutExpoといったjQueryのeasing指定可能
+  scrollSpeed: 300, // スクロール時の速度
+
+  //以下、ページネーション設定
+  before: function (i, panels) {
+    var ref = panels[i].attr("data-section-name");
+    $(".pagination .active").removeClass("active");
+    $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+  },
+  afterRender: function () {
+    var pagination = "<ul class=\"pagination\">";
+    var activeClass = "";
+    $(".box").each(function (i) {//1ページスクロールさせたいエリアクラス名を指定
+      activeClass = "";
+      if (i === $.scrollify.currentIndex()) {
+        activeClass = "active";
       }
-  
+      pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
     });
+    pagination += "</ul>";
+
+    $("#box1").append(pagination);//はじめのエリアにページネーションを表示
+    $(".pagination a").on("click", $.scrollify.move);
+  }
+
+});
+
+//写真スライドショー
+$('.slider').slick({
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 3,//スライドを画面に3枚見せる
+  slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+  dots: true,//下部ドットナビゲーションの表示
+  responsive: [
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 2,//スライドを画面に2枚見せる
+      slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+    }
+  },
+  {
+    breakpoint: 426,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 1,//スライドを画面に1枚見せる
+      slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    }
+  }
+]
+});
